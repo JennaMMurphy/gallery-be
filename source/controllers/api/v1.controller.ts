e1922f97b6acec3a1833e61edd16d73154a2e02b
JennaMMurphy <blue-marie5@hotmail.com>
2024-08-24 08:16:02 -0700

    feat: adds gallery items endpoint

--- source/controllers/api/v1.controller.ts
+++ source/controllers/api/v1.controller.ts
@@ -1,15 +1,32 @@
-import { Request, Response, NextFunction } from "express";
-import axios, { AxiosResponse } from "axios";
+import { Request, Response } from "express";
 
-const testFunction = async (
+export const galleryItems = async (
   request: Request,
-  response: Response,
-  next: NextFunction
+  response: Response
 ) => {
-  const result: AxiosResponse = await axios.get(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  );
-  return response.send(result.data);
+ const result = [
+    {
+      description: "A description",
+      price: 1000.42,
+      src: "https://images.pexels.com/photos/2167395/pexels-photo-2167395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "Name of art piece",
+    },
+    {
+      description: "a description",
+      price: 1000.42,
+      src: "https://cdn.tutsplus.com/cdn-cgi/image/width=1200/psd/uploads/legacy/0576_Greek_Statue/final.jpg",
+      title: " a name",
+    },
+    {
+      description: "Placeholder Image",
+      price: 1000.42,
+      src: "https://miro.medium.com/v2/resize:fit:1100/format:webp/0*bQU5tpVvN6wEdqNv.jpg",
+      title: "the name of the piece",
+    },
+  ];
+  return response.send(result);
 };
 
-export default testFunction;
+export default {
+  galleryItems
+}
--- source/routes/api/v1.ts
+++ source/routes/api/v1.ts
@@ -2,6 +2,6 @@ import express from 'express'
 import controller from '../../controllers/api/v1.controller'
 const router = express.Router()
 
-router.get('/testRoute', controller)
+router.get('/gallery/items', controller.galleryItems)
 
 export default router
